35052e4c0fdb741d56e265e1ae736bca377a32f8
Flavio-Converso <fla.converso@gmail.com>
2024-05-20 16:37:47 +0200

    .

--- primaSettimana/20-05/script.js
+++ primaSettimana/20-05/script.js
@@ -1,1 +1,17 @@
 "use strict";
+class User {
+  constructor(_nome, _cognome) {
+    this.credito = 0;
+    this.numeroChiamate = 0;
+    this.costoMinuto = 0.2;
+    this.nome = _nome;
+    this.cognome = _cognome;
+  }
+  ricarica(n) {
+    this.credito += n;
+  }
+}
+const user1 = new User("Mario", "Rossi");
+console.log("credito pre-ricarica: " + user1.credito);
+user1.ricarica(10);
+console.log("credito post-ricarica " + user1.credito);
--- primaSettimana/20-05/script.ts
+++ primaSettimana/20-05/script.ts
@@ -4,10 +4,21 @@ interface iSmartPhone{
 }
 
 class User implements iSmartPhone{
-    credito:number;
-    numeroChiamate:number;
-    constructor(credito:number,numeroChiamate:number){
-        this.credito=credito;
-        this.numeroChiamate=numeroChiamate;
+    credito: number= 0;
+    numeroChiamate: number = 0;
+    costoMinuto:number = 0.2;
+    nome:string;
+    cognome:string;
+    constructor(_nome:string,_cognome:string){
+        this.nome=_nome;
+        this.cognome=_cognome;
+    }
+    ricarica(n:number){
+        this.credito+=n;
     }
 }
+const user1 = new User("Mario","Rossi");
+console.log("credito pre-ricarica: "+user1.credito);
+user1.ricarica(10);
+
+console.log("credito post-ricarica "+user1.credito);

fcae07122b50295f93d0df9f61bdb05aa06afb07
Flavio-Converso <fla.converso@gmail.com>
2024-05-20 16:58:14 +0200

    .

--- primaSettimana/20-05/script.js
+++ primaSettimana/20-05/script.js
@@ -1,18 +1,26 @@
 "use strict";
 class User {
-  constructor(_nome, _cognome) {
-    this.credito = 0;
-    this.numeroChiamate = 0;
-    this.costoMinuto = 0.2;
-    this.nome = _nome;
-    this.cognome = _cognome;
-  }
-  ricarica(n) {
-    this.credito += n;
-  }
+    constructor(_nome, _cognome) {
+        this.credito = 0;
+        this.numeroChiamate = 0;
+        this.costoMinuto = 0.2;
+        this.nome = _nome;
+        this.cognome = _cognome;
+    }
+    ricarica(n) {
+        this.credito += n;
+    }
+    chiamata(minutiChiamata) {
+        let costo = this.costoMinuto * minutiChiamata;
+        this.credito -= costo;
+        this.numeroChiamate += minutiChiamata;
+    }
+    get chiama404() {
+        return "il credito reisduo è di: " + this.credito + "€";
+    }
 }
 const user1 = new User("Mario", "Rossi");
 console.log("credito pre-ricarica: " + user1.credito);
-
 user1.ricarica(10);
-console.log("credito post-ricarica " + user1.credito);
+user1.chiamata(5);
+console.log(user1.chiama404);

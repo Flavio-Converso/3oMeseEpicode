8599fa812646f4c17960b47235ce0618bd06dba8
Flavio-Converso <fla.converso@gmail.com>
2024-06-03 17:26:07 +0200

    .

--- terzaSettimana/03-06/src/app/homepage/homepage.module.ts
+++ terzaSettimana/03-06/src/app/homepage/homepage.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
+
 import { SharedModule } from '../shared.module';
-import { SinglePostComponent } from '../single-post/single-post.component';
 
 @NgModule({
-  declarations: [HomepageComponent, SinglePostComponent],
+  declarations: [HomepageComponent],
   imports: [CommonModule, HomepageRoutingModule, SharedModule],
 })
 export class HomepageModule {}
--- terzaSettimana/03-06/src/app/shared.module.ts
+++ terzaSettimana/03-06/src/app/shared.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SinglePostComponent } from './single-post/single-post.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [SinglePostComponent],
   imports: [CommonModule, FormsModule],
-  exports: [FormsModule],
+  exports: [SinglePostComponent, FormsModule],
 })
 export class SharedModule {}

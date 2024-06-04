775ef743b9a45e68a898ccc74de0aad5358358a5
Flavio-Converso <fla.converso@gmail.com>
2024-06-05 01:30:09 +0200

    validator

--- terzaSettimana/04-06/src/app/components/registrazione/registrazione.component.ts
+++ terzaSettimana/04-06/src/app/components/registrazione/registrazione.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorMailPasswordService } from '../../validator-mail-password.service';
 
 @Component({
   selector: 'app-registrazione',
@@ -10,16 +11,35 @@ export class RegistrazioneComponent {
   registrazioneForm!: FormGroup;
   formSubmitted = false;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private validatorSvc: ValidatorMailPasswordService // Inietta il servizio
+  ) {}
 
   ngOnInit() {
     this.registrazioneForm = this.fb.group({
       nome: [null, [Validators.required]],
       cognome: [null, [Validators.required]],
-      authData: this.fb.group({
-        email: [null, [Validators.required, Validators.email]],
-        password: [null, [Validators.required]],
-      }),
+      authData: this.fb.group(
+        {
+          email: [
+            null,
+            [Validators.required, this.validatorSvc.isValidEmail()],
+          ], // Richiama la funzione di validazione per l'email
+          confermaEmail: [null, [Validators.required]],
+          password: [
+            null,
+            [Validators.required, this.validatorSvc.isValidPassword()],
+          ], // Richiama la funzione di validazione per la password
+          confermaPassword: [null, [Validators.required]],
+        },
+        {
+          validators: [
+            this.validatorSvc.emailMatch,
+            this.validatorSvc.passwordMatch,
+          ], // Richiama le funzioni di validazione per il match tra email e tra password
+        }
+      ),
       genere: ['selectPlaceholder', [Validators.required]],
       immagine: [null],
       biografia: [
